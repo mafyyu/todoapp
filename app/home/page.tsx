97d018560a33aa8690c940594bb289cb0e41bf30
mafyyu <morita0126m@gmail.com>
2024-12-24 21:46:39 +0900

    feat:homeページの作成

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+import React from 'react';
+
+const Home = ()=>{
+    return (
+        <>
+        <Header></Header>
+        </>
+    )
+};
+
+export default Home;
