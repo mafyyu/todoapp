ae9c6fff542c8c9405e72bbe5db5d31dff1e7347
mafyyu <morita0126m@gmail.com>
2024-12-24 22:44:14 +0900

    feat:ファイルの作成

---
+++ app/components/Title.tsx
@@ -0,0 +1,7 @@
+export function Title (){
+    return (
+        <>
+        <h1 className="text-3xl font-semibold text-center mb-6">現在のタスク</h1>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Google from "./pages/LoginPage";
 import { Header } from "./components/Header";
 import { TaskForm } from "./components/TaskForm";
+import { Title } from "./components/Title";
 export default function Home() {
   return(
     <>
