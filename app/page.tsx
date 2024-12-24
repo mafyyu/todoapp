6aa298cb2df33e15712ad2ebb6777ce256668110
mafyyu <morita0126m@gmail.com>
2024-12-24 20:03:18 +0900

    import分の変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Google from "./pages/LoginPage";
 import { Header } from "./components/Header";
-import { TaskForm } from "./components/TaskForm ";
+import { TaskForm } from "./components/TaskForm";
 export default function Home() {
   return(
     <>
